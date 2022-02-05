efad80da14a99e11f957ffe6fca9c0d21ea47f39
prajeshJugga <prajesh.jugga@gmail.com>
2022-02-05 13:18:11 +0200

    Add project files.

---
+++ MatchResultsProcessor/FileObjects/GameResultsFile.cs
@@ -0,0 +1,12 @@
+﻿using MatchResultsProcessor.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatchResultsProcessor.FileObjects
+{
+    public class GameResultsFile : IFileObject
+    {
+        public string GameResult { get; set; }
+    }
+}
---
+++ MatchResultsProcessor/Interfaces/IFileReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatchResultsProcessor.Interfaces
+{
+    internal interface IFileReader
+    {
+        List<IFileObject> GetFileObjects();
+    }
+}
