7e3b4f6b0fd28b782ee2c4fef841f1d4997aa09d
prajeshJugga <prajesh.jugga@gmail.com>
2022-02-05 22:37:57 +0200

    Added tests for processing file lines into game result objects

---
+++ FileProcessorTests/FileProcessorTests.cs
@@ -0,0 +1,118 @@
+using MatchResultsProcessor.DTOs;
+using MatchResultsProcessor.FileObjects;
+using MatchResultsProcessor.FileProcessorService;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace FileProcessorTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Successfully_Extracts_GameResult_From_FileObject()
+        {
+            // Arrange
+            GameResultProcessor gameResultProcessor = new GameResultProcessor();
+            List<GameResultLine> gameResultLines = GetGameResultLines();
+            List<GameDetailsDTO> expectedResults = GetExpectedResults();
+            // Act
+            List<GameDetailsDTO> gameDetailsList = gameResultProcessor.ProcessFile(gameResultLines);
+            // Assert
+            Assert.AreEqual(expectedResults.Count, gameDetailsList.Count);
+            for (int i = 0; i < gameDetailsList.Count; i++)
+            {
+                Assert.AreEqual(expectedResults[i], gameResultLines[i]);
+            }
+        }
+
+        private List<GameDetailsDTO> GetExpectedResults()
+        {
+            return new List<GameDetailsDTO>
+            {
+                new GameDetailsDTO
+                {
+                     TeamA = new TeamStatisticDTO
+                     {
+                         TeamName = "Lions",
+                         GoalsScored = 3
+                     },
+                     TeamB = new TeamStatisticDTO
+                     {
+                         TeamName = "Snakes",
+                         GoalsScored = 3
+                     }
+                },
+                new GameDetailsDTO
+                {
+                     TeamA = new TeamStatisticDTO
+                     {
+                         TeamName = "Tarantulas",
+                         GoalsScored = 1
+                     },
+                     TeamB = new TeamStatisticDTO
+                     {
+                         TeamName = "FC Awesome",
+                         GoalsScored = 0
+                     }
+                },
+                new GameDetailsDTO
+                {
+                     TeamA = new TeamStatisticDTO
+                     {
+                         TeamName = "Lions",
+                         GoalsScored = 1
+                     },
+                     TeamB = new TeamStatisticDTO
+                     {
+                         TeamName = "FC Awesome",
+                         GoalsScored = 1
+                     }
+                },
+                new GameDetailsDTO
+                {
+                     TeamA = new TeamStatisticDTO
+                     {
+                         TeamName = "Tarantulas",
+                         GoalsScored = 3
+                     },
+                     TeamB = new TeamStatisticDTO
+                     {
+                         TeamName = "Snakes",
+                         GoalsScored = 1
+                     }
+                },
+                new GameDetailsDTO
+                {
+                     TeamA = new TeamStatisticDTO
+                     {
+                         TeamName = "Lions",
+                         GoalsScored = 4
+                     },
+                     TeamB = new TeamStatisticDTO
+                     {
+                         TeamName = "Grouches",
+                         GoalsScored = 0
+                     }
+                }
+            };
+        }
+
+        private List<GameResultLine> GetGameResultLines()
+        {
+            return new List<GameResultLine>()
+            {
+                new GameResultLine { GameResult = "Lions 3, Snakes 3" },
+                new GameResultLine { GameResult = "Tarantulas 1, FC Awesome 0" },
+                new GameResultLine { GameResult = "Lions 1, FC Awesome 1" },
+                new GameResultLine { GameResult= "Tarantulas 3, Snakes 1" },
+                new GameResultLine { GameResult= "Lions 4, Grouches 0" }
+            };
+        }
+    }
+}
--- FileReaderTests/GameResultsReaderTests.cs
+++ FileReaderTests/GameResultsReaderTests.cs
@@ -22,10 +22,10 @@ namespace FileReaderTests
             _directoryPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
         }
 
-        public void Read_And_Deserialize_A_File(string filePath, int expectedLineCount, List<GameResultsLine> expectedGameResultsLines)
+        public void Read_And_Deserialize_A_File(string filePath, int expectedLineCount, List<GameResultLine> expectedGameResultsLines)
         {
             // Arrange
-            List<GameResultsLine> gameResultsFiles = new List<GameResultsLine>();
+            List<GameResultLine> gameResultsFiles = new List<GameResultLine>();
             // string filePath = Path.Join(Directory.GetCurrentDirectory(), "SampleFiles", "ValidFile5Results.txt");
             // Act
             gameResultsFiles = _gameResultFileReaderService.GetFileObjects(filePath);
@@ -44,20 +44,20 @@ namespace FileReaderTests
         {
             // Arrange
             string filePath = Path.Join(Directory.GetCurrentDirectory(), "SampleFiles", "ValidFile5Results.txt");
-            List<GameResultsLine> expectedGameResultsLines = GetExpectedResultsForSmallFile();
+            List<GameResultLine> expectedGameResultsLines = GetExpectedResultsForSmallFile();
             // Act & Assert
             Read_And_Deserialize_A_File(filePath, 5, expectedGameResultsLines);
         }
 
-        private List<GameResultsLine> GetExpectedResultsForSmallFile()
+        private List<GameResultLine> GetExpectedResultsForSmallFile()
         {
-            return new List<GameResultsLine>()
+            return new List<GameResultLine>()
             {
-                new GameResultsLine { GameResult = "Lions 3, Snakes 3" },
-                new GameResultsLine { GameResult = "Tarantulas 1, FC Awesome 0" },
-                new GameResultsLine { GameResult = "Lions 1, FC Awesome 1" },
-                new GameResultsLine { GameResult= "Tarantulas 3, Snakes 1" },
-                new GameResultsLine { GameResult= "Lions 4, Grouches 0" }
+                new GameResultLine { GameResult = "Lions 3, Snakes 3" },
+                new GameResultLine { GameResult = "Tarantulas 1, FC Awesome 0" },
+                new GameResultLine { GameResult = "Lions 1, FC Awesome 1" },
+                new GameResultLine { GameResult= "Tarantulas 3, Snakes 1" },
+                new GameResultLine { GameResult= "Lions 4, Grouches 0" }
             };
         }
     }
