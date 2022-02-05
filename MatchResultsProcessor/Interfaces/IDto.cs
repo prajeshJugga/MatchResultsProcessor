667187b3caf7cbf0084af62bde23e756b595119d
prajeshJugga <prajesh.jugga@gmail.com>
2022-02-05 19:41:34 +0200

    Addedfile processor interface and dto interface

---
+++ MatchResultsProcessor/Interfaces/IDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatchResultsProcessor.Interfaces
+{
+    internal interface IDto
+    {
+    }
+}
---
+++ MatchResultsProcessor/Interfaces/IFileProcessorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatchResultsProcessor.Interfaces
+{
+    internal interface IFileProcessorService<InputObject, OutputObject> 
+        where InputObject : IFileObject
+        where OutputObject : IDto
+    {
+        List<OutputObject> GetOutputObjects(List<InputObject> inputObjects);
+    }
+}
